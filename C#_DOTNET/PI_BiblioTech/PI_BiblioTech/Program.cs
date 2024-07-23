22ab13f3807c529fabe624189a33833ee4c6c2fe
fernandaatim <143547756+fernandaatim@users.noreply.github.com>
2024-07-22 21:39:41 -0300

    Projeto Integrador - Conclusão de Curso

---
+++ C#_DOTNET/PI_BiblioTech/PI_BiblioTech/Program.cs
@@ -0,0 +1,13 @@
+
+using view;
+
+namespace Programa;
+
+static class Program
+{
+    public static void Main()
+    {
+        Application.Run(new TelaFuncionario());
+        TelaFuncionario menu = new TelaFuncionario();
+        }
+}
---
+++ C#_DOTNET/PI_BiblioTech/PI_BiblioTech/Repo/FuncionarioRepo.cs
@@ -0,0 +1,6 @@
+using model;
+namespace repo;
+public class FuncionarioRepo
+{
+    public static List<Funcionario> funcionario = new List<Funcionario>();
+}
